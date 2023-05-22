6fdd54506703b7971919919525f2f5c7b9dec939
WinterHana <haha920@naver.com>
2023-05-22 18:32:52 +0900

    Refactor : API 명세서에 맞게 다시 수정
    - 명세서에 맞게 코드를 다시 리팩토링 했습니다.
    
    related #14

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,19 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restx import Api, Resource
 
 from apis.image_similarity_visual import compare
 from apis.problem import problem
 
 app = Flask(__name__)
+
 api = Api(
     app,
-    version='0.1',
+    version='0.2',
     title='fps project API server',
     description='fps project를 위한 서버입니다.'
     )
 
-api.add_namespace(compare, '/compare')
-api.add_namespace(problem, '/problems')
+api.add_namespace(compare, '/compare')      # 이미지 유사도 관련
+api.add_namespace(problem, '/problems')     # 문제 관련
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
